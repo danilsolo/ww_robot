5d9f392de0afbdd414e14a180669a7ff08824de9
danilsolo <danilsolo123@gmail.com>
2017-11-20 05:24:24 +0300

    adding logging in file

--- ww_robot_wh.py
+++ ww_robot_wh.py
@@ -81,7 +81,7 @@ salfetka = '''
 
 BOTCHAT = 76201733
 logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)-3s]# %(levelname)-5s [%(asctime)s] %(message)s'
-                    , level=logging.INFO, filename='ww.log')
+                    , level=logging.INFO)
 
 # создаём logger
 logger = logging.getLogger()
